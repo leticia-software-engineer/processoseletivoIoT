9b72eb9ada01425423122abba3a3eb12390635bb
Leticia Dias <devv.leticia@gmail.com>
2026-04-19 12:52:57 -0300

    feat: initial firmware semafor

--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
-print("Teste")
+from machine import Pin
+import time
+
+leds = [
+    Pin(22, Pin.OUT),  # vermelho
+    Pin(19, Pin.OUT),  # amarelo
+    Pin(4, Pin.OUT)    # verde
+]
+
+def apagar_todos():
+    for led in leds:
+        led.value(0)
+
+def acender(led, tempo):
+    apagar_todos()
+    led.value(1)
+    time.sleep(tempo)
+
+# estado inicial
+apagar_todos()
+
+while True:
+    acender(leds[0], 5)
+    acender(leds[2], 5)
+    acender(leds[1], 2)
